a1f5e452379af64db5ef4fdd52b4113ae81d066e
fleonardolewis@gmail.com <fleonardolewis@gmail.com>
2023-04-30 16:30:04 -0500

    lab

--- scikit-learn-regression.py
+++ scikit-learn-regression.py
@@ -1,3 +1,5 @@
+# found here https://harvard-iacs.github.io/2019-CS109A/labs/lab-3/solutions/
+
 import requests
 from IPython.core.display import HTML
 styles = requests.get("https://raw.githubusercontent.com/Harvard-IACS/2018-CS109A/master/content/styles/cs109.css").text
@@ -14,9 +16,144 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import pandas as pd
 import time
-pd.set_option('display.width', 500)
-pd.set_option('display.max_columns', 100)
-pd.set_option('display.notebook_repr_html', True)
 import seaborn as sns
 
+x_train = np.array([1,2,3])
+y_train = np.array([2,3,6])
 
+
+x_train = x_train.reshape(3,1)
+xx = np.array([[1,3,5],[6,2,1]])
+xx = xx.reshape(3,-1)
+
+x_train = np.array([1,2,3])
+y_train = np.array([2,2,4])
+
+
+
+def nice_scatterplot(x, y, title):
+    # font size
+    f_size = 18
+    
+    # make the figure
+    fig, ax = plt.subplots(1,1, figsize=(8,5)) # Create figure object
+
+    # set axes limits to make the scale nice
+    ax.set_xlim(np.min(x)-1, np.max(x) + 1)
+    ax.set_ylim(np.min(y)-1, np.max(y) + 1)
+
+    # adjust size of tickmarks in axes
+    ax.tick_params(labelsize = f_size)
+    
+    # remove tick labels
+    ax.tick_params(labelbottom=False,  bottom=False)
+    
+    # adjust size of axis label
+    ax.set_xlabel(r'$x$', fontsize = f_size)
+    ax.set_ylabel(r'$y$', fontsize = f_size)
+    
+    # set figure title label
+    ax.set_title(title, fontsize = f_size)
+
+    # you may set up grid with this 
+    #ax.grid(True, lw=1.75, ls='--', alpha=0.15)
+
+    # make actual plot (Notice the label argument!)
+    #ax.scatter(x, y, label=r'$my points$')
+    #ax.scatter(x, y, label='$my points$')
+    ax.scatter(x, y, label=r'$my\,points$')
+    ax.legend(loc='best', fontsize = f_size)
+    plt.show()
+    return fig
+
+nice_scatterplot(x_train, y_train, 'hello nice plot')
+
+
+
+
+# Reshape to be a proper 2D array
+x_train = x_train.reshape(x_train.shape[0], 1)
+y_train = y_train.reshape(y_train.shape[0], 1)
+
+print(x_train.shape)
+
+(3, 1)
+
+# first, compute means
+y_bar = np.mean(y_train)
+x_bar = np.mean(x_train)
+
+# build the two terms
+numerator = np.sum( (x_train - x_bar)*(y_train - y_bar) )
+denominator = np.sum((x_train - x_bar)**2)
+print(numerator.shape, denominator.shape) #check shapes
+
+#slope beta1
+beta_1 = numerator/denominator
+
+#intercept beta0
+beta_0 = y_bar - beta_1*x_bar
+
+print("The best-fit line is {0:3.2f} + {1:3.2f} * x".format(beta_0, beta_1))
+print(f'The best fit is {beta_0}')
+def simple_linear_regression_fit(x_train: np.ndarray, y_train: np.ndarray) -> np.ndarray:
+    """
+    Inputs:
+    x_train: a (num observations by 1) array holding the values of the predictor variable
+    y_train: a (num observations by 1) array holding the values of the response variable
+
+    Returns:
+    beta_vals:  a (num_features by 1) array holding the intercept and slope coeficients
+    """
+    
+    # Check input array sizes
+    if len(x_train.shape) < 2:
+        print("Reshaping features array.")
+        x_train = x_train.reshape(x_train.shape[0], 1)
+
+    if len(y_train.shape) < 2:
+        print("Reshaping observations array.")
+        y_train = y_train.reshape(y_train.shape[0], 1)
+
+    # first, compute means
+    y_bar = np.mean(y_train)
+    x_bar = np.mean(x_train)
+
+    # build the two terms
+    numerator = np.sum( (x_train - x_bar)*(y_train - y_bar) )
+    denominator = np.sum((x_train - x_bar)**2)
+    
+    #slope beta1
+    beta_1 = numerator/denominator
+
+    #intercept beta0
+    beta_0 = y_bar - beta_1*x_bar
+
+    return np.array([beta_0,beta_1])
+
+
+
+x_train = np.array([1 ,2, 3])
+y_train = np.array([2, 2, 4])
+
+betas = simple_linear_regression_fit(x_train, y_train)
+
+beta_0 = betas[0]
+beta_1 = betas[1]
+
+print("The best-fit line is {0:8.6f} + {1:8.6f} * x".format(beta_0, beta_1))
+
+fig_scat, ax_scat = plt.subplots(1,1, figsize=(10,6))
+
+# Plot best-fit line
+x_train = np.array([[1, 2, 3]]).T
+
+best_fit = beta_0 + beta_1 * x_train
+
+ax_scat.scatter(x_train, y_train, s=300, label='Training Data')
+ax_scat.plot(x_train, best_fit, ls='--', label='Best Fit Line')
+
+ax_scat.set_xlabel(r'$x_{train}$')
+ax_scat.set_ylabel(r'$y$')
+
+plt.show()
