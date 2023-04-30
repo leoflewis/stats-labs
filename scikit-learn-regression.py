e8acfbd6aecd2e0101ec75e2bc34fb7304020c89
fleonardolewis@gmail.com <fleonardolewis@gmail.com>
2023-04-30 15:47:42 -0500

    inital commit

---
+++ scikit-learn-regression.py
@@ -0,0 +1,22 @@
+import requests
+from IPython.core.display import HTML
+styles = requests.get("https://raw.githubusercontent.com/Harvard-IACS/2018-CS109A/master/content/styles/cs109.css").text
+HTML(styles)
+
+
+# import the necessary libraries
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import scipy as sp
+import matplotlib as mpl
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import pandas as pd
+import time
+pd.set_option('display.width', 500)
+pd.set_option('display.max_columns', 100)
+pd.set_option('display.notebook_repr_html', True)
+import seaborn as sns
+
+
